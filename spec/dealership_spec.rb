bd26055639660804b31e39d5b3f60719e05cfd31
margdaly <margaret.daly@gmail.com>
2023-02-08 10:30:12 -0700

    test: Add iteration 3 tests

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -3,50 +3,130 @@ require './lib/car'
 require './lib/dealership'
 
 RSpec.describe Dealership do
-  it 'exists' do
-    dealership = Dealership.new("Acme Auto", "123 Main Street")
+  describe 'exists' do
+    it 'exists' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
 
-    expect(dealership).to be_a Dealership
+      expect(dealership).to be_a Dealership
+    end
+
+    it 'has inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.inventory).to eq([])
+    end
+
+    it 'counts inventory' do 
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.inventory_count).to eq(0)
+    end
+
+    it 'can add a car' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      dealership.add_car(car_1)
+
+      expect(dealership.inventory).to eq([car_1])
+    end
+
+    it 'can add cars' do 
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+
+      expect(dealership.inventory).to eq([car_1, car_2])
+    end
+    
+    it 'can count inventory again' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+
+      expect(dealership.inventory_count).to eq(2)
+    end
   end
 
-  it 'has inventory' do
-    dealership = Dealership.new("Acme Auto", "123 Main Street")
+  describe 'knows the buisness' do
+    it 'has zero inventory info' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
 
-    expect(dealership.inventory).to eq([])
-  end
+      expect(dealership.has_inventory?).to eq(false)
+    end
 
-  it 'counts inventory' do 
-    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    it 'has inventory info' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
 
-    expect(dealership.inventory_count).to eq(0)
-  end
+      expect(dealership.has_inventory?).to eq(true)
+    end
 
-  it 'can add a car' do
-    dealership = Dealership.new("Acme Auto", "123 Main Street")
-    car_1 = Car.new("Ford Mustang", 1500, 36)
-    car_2 = Car.new("Toyota Prius", 1000, 48)
-    dealership.add_car(car_1)
+    it 'knows cars by make' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
 
-    expect(dealership.inventory).to eq([car_1])
-  end
+      expect(dealership.cars_by_make("Toyota")).to eq([car_2, car_3])
+    end
 
-  it 'can add cars' do 
-    dealership = Dealership.new("Acme Auto", "123 Main Street")
-    car_1 = Car.new("Ford Mustang", 1500, 36)
-    car_2 = Car.new("Toyota Prius", 1000, 48)
-    dealership.add_car(car_1)
-    dealership.add_car(car_2)
+    it 'knows more than one make' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
 
-    expect(dealership.inventory).to eq([car_1, car_2])
-  end
-  
-  it 'can count inventory again' do
-    dealership = Dealership.new("Acme Auto", "123 Main Street")
-    car_1 = Car.new("Ford Mustang", 1500, 36)
-    car_2 = Car.new("Toyota Prius", 1000, 48)
-    dealership.add_car(car_1)
-    dealership.add_car(car_2)
+      expect(dealership.cars_by_make("Ford")).to eq([car_1])
+    end
 
-    expect(dealership.inventory_count).to eq(2)
+    it 'has a total value' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+
+      expect(dealership.total_value).to eq(156000)
+    end
+
+    it 'has details' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+
+      expect(dealership.details).to eq({"total_value" => 156000, "address" => "123 Main Street"})
+    end
   end
 end
