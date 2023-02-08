7ff2989746dadf8ecaed06bc929a2351553332ff
margdaly <margaret.daly@gmail.com>
2023-02-08 10:12:26 -0700

    feat: Add inventory methods to dealership

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,7 +1,22 @@
 class Dealership
+  attr_accessor :inventory, :inventory_count
 
   def initialize(name, address)
     @name = name
     @address = address
+    @inventory = []
+    @inventory_count = 0
+  end
+
+  def inventory
+    @inventory
+  end
+
+  def inventory_count
+    @inventory.length
+  end
+
+  def add_car(car)
+    @inventory << car
   end
 end
